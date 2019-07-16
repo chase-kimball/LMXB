861d7c55bcef2713548c576a4bbdd04e943005f2
shrujal123 <31377760+shrujal123@users.noreply.github.com>
2019-07-16 14:05:50 -0500

    Update Observed.py

--- Observed.py
+++ Observed.py
@@ -115,3 +115,46 @@ def getSysDict(sysname):
 
     system = Sys_Dict[sysname]
     return system
+
+import Observed #dictionary file
+import TrajTools
+from astropy import units as u
+import numpy as np
+
+gauss=np.random.normal
+
+import scipy.stats as stats
+from astropy.coordinates import SkyCoord
+import astropy.coordinates as coord
+from astropy import constants as C
+
+Msun=C.M_sun.value
+m_kpc=u.kpc.to(u.m)
+
+
+## Class for systems
+
+class LMXB_Sys:
+    def __init__(self, sysname):
+        self.system = Observed.getSysDict(sysname)
+        
+        ## Assigns the parameters of each system to a variable
+        self.l = self.system.get('l')
+        self.b = self.system.get('b')
+        self.d = self.system.get('d')
+        self.pm_ra = self.system.get('pm_ra')
+        self.pm_dec = self.system.get('pm_dec')
+        self.v_rad = self.system.get('v_rad')
+        
+        ## Converts into earth centric coordinates
+        Sys_gal = SkyCoord(l = self.system.get('l')*u.deg, b = self.system.get('b')*u.deg, frame = 'galactic')
+        self.ra = Sys_gal.icrs.ra.value
+        self.dec = Sys_gal.icrs.dec.value
+         
+        
+        
+    def getCartesian(self):
+        self.X0, self.Y0, self.Z0, self.U0, self.V0, self.W0 = TrajTools.getXYZUVW(self.ra, self.dec, self.d, self.pm_ra, self.pm_dec, self.v_rad)
+        return self.X0, self.Y0, self.Z0, self.U0, self.V0, self.W0 
+    
+    
