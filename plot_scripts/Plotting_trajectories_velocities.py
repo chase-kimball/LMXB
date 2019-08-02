378fbd09d92c247061c92105c961905834cd7ae4
SPEEDCat800 <52935795+SPEEDCat800@users.noreply.github.com>
2019-08-02 14:03:42 -0500

    Update Plotting_trajectories_velocities.py

--- plot_scripts/Plotting_trajectories_velocities.py
+++ plot_scripts/Plotting_trajectories_velocities.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+	# Copyright (C) Shrujal Ambati, Richard Ackermann, Chase Kimball (2019)
+	#
+	# This file is part of the LMXB package
+	#
+	# LMXB is free software: you can redistribute it and/or modify
+	# it under the terms of the GNU General Public License as published by
+	# the Free Software Foundation, either version 3 of the License, or
+	# (at your option) any later version.
+	#
+	# progenitor is distributed in the hope that it will be useful,
+	# but WITHOUT ANY WARRANTY; without even the implied warranty of
+	# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	# GNU General Public License for more details.
+	#
+	# You should have received a copy of the GNU General Public License
+	# along with LMXB.  If not, see <http://www.gnu.org/licenses/>.
+	
+	__author__ = ['Shrujal Ambati','Richard Ackermann','Chase Kimball']
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
