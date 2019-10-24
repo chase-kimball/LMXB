4b68a1b3484f06e7fa72775b51b33a95fdce82a9
charles.kimball <charles.kimball@ligo.org>
2019-10-24 13:37:51 -0500

    updated interactive.py

--- plot_scripts/interactive.py
+++ plot_scripts/interactive.py
@@ -1,20 +1,20 @@
 import numpy as np
 import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.widgets import CheckButtons
 import astropy.units as units
 import sys
 sys.path.append('..')
 import eccentricPre as SN
-
+import pandas as pd
+import pdb
 inputs = 50
 threshold = 1
 Mcomp = 20
 Mhe = 5
 
 ap = np.linspace(20, 70, inputs)
-ep = np.linspace(20, 70, inputs)
+ep = np.linspace(0, .99, inputs)
 
 apx, epy = np.meshgrid(ap, ep)
 
@@ -26,11 +26,13 @@ data = []
 df = pd.DataFrame()
 
 for i in range(inputs**2):
+    print(Mcomp, Mhe, Apre, epre)
     sys = SN.System(Mcomp, Mhe, Apre[i], epre[i], nk)
     sys.SN()
     data.append({'Apre': sys.Apre[0], 'epre': sys.epre[0], 'oldSNflag1': np.sum(sys.oldSNflag1)>=threshold, 
                  'SNflag1': np.sum(sys.SNflag1)>=threshold, 'SNflag2': np.sum(sys.SNflag2)>=threshold,
-                 'SNflag3': np.sum(sys.SNflag3)>=threshold, 'SNflag4': np.sum(sys.SNflag4)>=threshold, 
+                 'SNflag3': np.sum(sys.SNflag3)>=threshold, 'SNflag4': np.sum(sys.SNflag4)>=threshold,
+                 'oldSNflag4': np.sum(sys.oldSNflag4)>=threshold,
                  'SNflag5': np.sum(sys.SNflag5)>=threshold, 'SNflag6': np.sum(sys.SNflag6)>=threshold, 
                  'SNflag7': np.sum(sys.SNflag7)>=threshold})
     
@@ -40,9 +42,9 @@ fig = plt.figure()
 ax = plt.subplot(1,1,1)
 
 rax = plt.axes([0.05, 0.4, 0.1, 0.15]) #play around til it's readable. These are just coordinates for the box where the buttons are displayed
-check = CheckButtons(rax, ('flag1', 'flag2', 'flag3', 'flag4', 'flag5', 'flag6', 'flag7'), (False) * 7) # Name the flags however you want and add as many as you need
+check = CheckButtons(rax, ('flag1', 'flag2', 'flag3', 'flag4', 'flag5', 'flag6', 'flag7'), [False] * 7) # Name the flags however you want and add as many as you need
 
-onPlot, = ax.scatter(df['Apre'], df['epre'],'g.',label='Pass')
+onPlot, = ax.plot(df['Apre'], df['epre'],'g.',label='Pass')
 test_dictionary = {
                    "flag1": {'on_off': False, 'flag_array': df['SNflag1']},
                    "flag2": {'on_off': False, 'flag_array': df['SNflag2']}, 
@@ -56,17 +58,24 @@ test_dictionary = {
 def func(label):
     global Ion
     global Ioff
-    
+    print(label)
+    print('before')
+    for key in test_dictionary.keys():
+        print (key, test_dictionary[key]['on_off'])
     test_dictionary[label]['on_off'] = not test_dictionary[label]['on_off']
-    
+    print('after')
+    for key in test_dictionary.keys():
+        print (key, test_dictionary[key]['on_off'])
     Ion = np.ones_like(df['Apre']) #(or like any of your arrays)
+
     for key in test_dictionary.keys():
         if test_dictionary[key]['on_off']:
+            print (key)
             Ion *= test_dictionary[key]['flag_array']
-
-    Ion = np.where(Ion)
-
-    onPlot.set_data(df['Apre'][Ion],df['Apre'][Ion])
+    Ion = np.where(Ion)[0]
+    print(df['Apre'].values[Ion])
+    onPlot.set_data(df['Apre'].values[Ion],df['epre'].values[Ion])
     
     plt.draw()
-check.on_clicked(func)    
+check.on_clicked(func)
+plt.show()
