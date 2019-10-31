78e011b343faa59e8d3f8650a35ca8c8658699de
Sam Imperato <samimperato@dhcp-10-105-43-154.wireless.northwestern.private>
2019-10-30 23:51:47 -0500

    updated docs and epreapre plotting script

--- plot_scripts/epreApre2d.py
+++ plot_scripts/epreApre2d.py
@@ -1,6 +1,8 @@
 import matplotlib
-matplotlib.use('Agg')
+matplotlib.use('TKAgg')
 import matplotlib.pyplot as plt
+import sys
+sys.path.append('..') 
 import eccentricPre as SN
 import pandas as pd
 import numpy as np
@@ -17,7 +19,7 @@ apx, epy = np.meshgrid(ap, ep)
 Apre = apx.flatten()
 epre = epy.flatten()
 
-nk = 500
+nk = 100
 data = []
 df = pd.DataFrame()
 
@@ -28,11 +30,11 @@ for i in range(inputs**2):
         data.append({'Apre': sys.Apre[n], 'epre': sys.epre[n], 'oldSNflag1': sys.oldSNflag1[n], 'SNflag1': sys.SNflag1[n], 'SNflag2': sys.SNflag2[n], 'SNflag3': sys.SNflag3[n], 'SNflag4': sys.SNflag4[n], 'SNflag5': sys.SNflag5[n], 'SNflag6': sys.SNflag6[n], 'SNflag7': sys.SNflag7[n], 'SNflags': np.all([item[n] for item in sys.SNflags])})
 df = pd.DataFrame(data)
 
-pdf = df[df['SNflags']]
+pdf = df[df['SNflag4'] == 1]
 print pdf['Apre']
 print df['Apre']
 plt.hist2d(pdf['Apre']*units.m.to(units.R_sun), pdf['epre'], bins=(50,50), cmap='viridis')
 plt.colorbar()
 plt.xlabel('ApreSN')
 plt.ylabel('EpreSN')
-plt.savefig('attempt')
+plt.show()
