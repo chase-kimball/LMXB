c06bc6d089bb56c867e7ab2726015549c3fffef6
Sam Imperato <samuelimperato2022@u.northwestern.edu>
2020-07-14 13:23:16 -0400

    added contours for which pass both

--- diverging.py
+++ diverging.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sn
 
 def get_params(system):
     sys_dict = Observed.getSysDict(system)
@@ -26,11 +27,10 @@ def calcP(Mbh_RLO, Mdon_RLO, P_RLO, alpha, beta, P_obs):
     q = 0
     
     while (P < P_obs and Mbh < Mbh_max):
-        Mbh += .1
         q = (Mdon_RLO - ((Mbh - Mbh_RLO)/((1-alpha)*(1-beta))))/Mbh
         P = (P_RLO * (((q * Mbh) / Mdon_RLO) ** (3 * (alpha-1))) * (((Mdon_RLO + Mbh_RLO)/((1+q)*Mbh))**2) * (Mbh_RLO/Mbh)**(3/(beta-1)))
-    
-    return Mbh, q
+        Mbh += .1
+    return Mbh - .1, q
 
 def distance(calc, obs, sigma):
     dist = (calc - obs)/sigma
@@ -40,11 +40,12 @@ def distance(calc, obs, sigma):
 system = 'V404'
 params = get_params(system)
 
-norm = matplotlib.colors.Normalize(vmin=-3.,vmax=3.)
-bounds = [-3, -2, -1, 0, 1, 2, 3]
+norm = matplotlib.colors.Normalize(vmin=-5.,vmax=5.)
+bounds = range(-5, 6)
+#bounds = [-4, -3, -2, -1, 0, 1, 2, 3, 4]
 
-#for n in range(2, 16):
-for n in np.arange(2, 16.5, .5):
+
+for n in range(2, 17):
     Mbh_RLO = n
 
     grid = make_2d_grid((0, 16, .2), (0, params[2], .125))
@@ -56,6 +57,7 @@ for n in np.arange(2, 16.5, .5):
 
     color_mbh = [[0 for r in range(h)] for c in range(w)] 
     color_q = [[0 for r in range(h)] for c in range(w)] 
+    passes = [[0 for r in range(h)] for c in range(w)]
 
     for i in range(w):
         for x in range(h):
@@ -64,28 +66,33 @@ for n in np.arange(2, 16.5, .5):
             color_mbh[i][x] = (distance(Mbh, params[0][0], params[0][1]))
             color_q[i][x] = (distance(q, params[1][0], params[1][1]))
 
+            if (abs(color_mbh[i][x]) <= 2 and abs(color_q[i][x]) <= 2):
+                passes[i][x] = 1
+
     fig, (ax1, ax2) = plt.subplots(1, 2)
     fig.set_size_inches(17, 8)
     fig.suptitle('Mbh_RLO: ' + str(n))
 
-    sc1 = ax1.pcolormesh(Mdon_RLO, P_RLO, color_mbh, cmap = 'Spectral_r', norm=norm)
-    cn1 = ax1.contour(Mdon_RLO, P_RLO, color_mbh, colors='k', levels=(-2, -1, 0, 1, 2), linestyles='solid')
-    ax1.clabel(cn1, inline=1, fontsize=10)
+    sc1 = ax1.pcolormesh(Mdon_RLO, P_RLO, color_mbh, cmap="RdBu_r", norm=norm)
+    cn1 = ax1.contour(Mdon_RLO, P_RLO, color_mbh, colors='k', levels=(-2, 0, 2), linestyles='solid')
+    ax1.contour(Mdon_RLO, P_RLO, passes, colors='w', levels=[1], linestyles='dashed')
+    ax1.clabel(cn1, inline=1, fontsize=10, fmt='%1.0f')
     ax1.set_title('Mbh deviation')
     ax1.set_xlabel('Mdon_RLO')
     ax1.set_ylabel('P_RLO')
     cb1 = fig.colorbar(sc1, ax = ax1, ticks = bounds, extend='both')
     cb1.set_label('standard deviations away')
 
-    sc2 = ax2.pcolormesh(Mdon_RLO, P_RLO, color_q, cmap='Spectral_r', norm=norm)
-    cn2 = ax2.contour(Mdon_RLO, P_RLO, color_q, colors='k', levels=(-2, -1, 0, 1, 2), linestyles='solid')
-    ax2.clabel(cn2, inline=1, fontsize=10)
+    sc2 = ax2.pcolormesh(Mdon_RLO, P_RLO, color_q, cmap="RdBu_r", norm=norm)
+    cn2 = ax2.contour(Mdon_RLO, P_RLO, color_q, colors='k', levels=(-2, 0, 2), linestyles='solid')
+    ax2.contour(Mdon_RLO, P_RLO, passes, colors='w', levels=[1], linestyles='dashed')
+    ax2.clabel(cn2, inline=1, fontsize=10, fmt='%1.0f')
     ax2.set_title('q deviation')
     ax2.set_xlabel('Mdon_RLO')
     ax2.set_ylabel('P_RLO')
     cb2 = fig.colorbar(sc2, ax = ax2, ticks = bounds, extend='both')
     cb2.set_label('standard deviations away')
     
-    plt.savefig('div_plots/fig' + str(n) + '.png', bbox_inches='tight')
+    plt.savefig('div_plots/fig' + str(n), bbox_inches='tight')
     print('plotted ' + str(n))
     plt.close()
