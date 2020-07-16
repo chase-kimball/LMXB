35144fd810ee08519cf83aa85b952d5adef622a0
Sam Imperato <samuelimperato2022@u.northwestern.edu>
2020-07-16 14:04:51 -0400

    updated to include beta=1 case

--- mbh_slice_plot.py
+++ mbh_slice_plot.py
@@ -14,85 +14,91 @@ def get_params(system):
 
     return Mbh, q, P
 
-def make_2d_grid(p1_range, p2_range):
-    param1 = np.arange(p1_range[0], p1_range[1], p1_range[2])
-    param2 = np.arange(p2_range[0], p2_range[1], p2_range[2])
-    p1, p2 = np.meshgrid(param1, param2)
-    return p1, p2
+def make_2d_grid(mdon_range, p_range):
+    mdon_all = np.arange(mdon_range[0], mdon_range[1], mdon_range[2])
+    p_all = np.arange(p_range[0], p_range[1], p_range[2])
+    mdon, p = np.meshgrid(mdon_all, p_all)
+    return mdon, p
 
 def calcP(Mbh_RLO, Mdon_RLO, P_RLO, alpha, beta, P_obs):
-    Mbh_max = Mbh_RLO + Mdon_RLO*(1-beta)*(1-alpha)
     P = P_RLO
     Mbh = Mbh_RLO
     q = 0
+
+    if (alpha == 1 or beta == 1):
+        Mdon = Mdon_RLO
+        Mdon_min = 0
+        q = 0
+        while True:#(P < P_obs and Mdon > Mdon_min):
+            q = Mdon / Mbh
+            P = ((P_RLO * (((q * Mbh) / Mdon_RLO) ** (3 * (alpha-1))) * (((Mdon_RLO + Mbh_RLO)/((1+q)*Mbh))**2)) * (math.exp((1-alpha)*(q-(Mdon_RLO/Mbh)))))
+            Mdon -= .1
+            if P > P_obs:
+                return Mdon + .1, q
+            elif Mdon < Mdon_min:
+                return (0, 0)
     
-    while (P < P_obs and Mbh < Mbh_max):
-        q = (Mdon_RLO - ((Mbh - Mbh_RLO)/((1-alpha)*(1-beta))))/Mbh
-        P = (P_RLO * (((q * Mbh) / Mdon_RLO) ** (3 * (alpha-1))) * (((Mdon_RLO + Mbh_RLO)/((1+q)*Mbh))**2) * (Mbh_RLO/Mbh)**(3/(beta-1)))
-        Mbh += .1
-    return Mbh - .1, q
+    else:
+        Mbh_max = Mbh_RLO + Mdon_RLO*(1-beta)*(1-alpha)
+        while (P < P_obs and Mbh < Mbh_max):
+            q = (Mdon_RLO - ((Mbh - Mbh_RLO)/((1-alpha)*(1-beta))))/Mbh
+            P = (P_RLO * (((q * Mbh) / Mdon_RLO) ** (3 * (alpha-1))) * (((Mdon_RLO + Mbh_RLO)/((1+q)*Mbh))**2) * (Mbh_RLO/Mbh)**(3/(beta-1)))
+            Mbh += .1
+        return Mbh - .1, q
 
 def distance(calc, obs, sigma):
     dist = (calc - obs)/sigma
     
     return dist
         
-system = 'V404'
-params = get_params(system)
 
-norm = matplotlib.colors.Normalize(vmin=-5.,vmax=5.)
-bounds = range(-5, 6)
-#bounds = [-4, -3, -2, -1, 0, 1, 2, 3, 4]
+def plot(Mbh_RLO):
+    Mdon_RLO, P_RLO = make_2d_grid((0, 16, .2), (0, params[2], .125))
+    w = len(P_RLO)
+    h = len(P_RLO[0])
 
-
-for n in range(2, 17):
-    Mbh_RLO = n
-
-    grid = make_2d_grid((0, 16, .2), (0, params[2], .125))
-    Mdon_RLO = grid[0]
-    P_RLO = grid[1]
-    
-    w = len(Mdon_RLO)
-    h = len(Mdon_RLO[0])
-
-    color_mbh = [[0 for r in range(h)] for c in range(w)] 
-    color_q = [[0 for r in range(h)] for c in range(w)] 
+    color_mbh = [[0 for r in range(h)] for c in range(w)]
+    color_q = [[0 for r in range(h)] for c in range(w)]
     passes = [[0 for r in range(h)] for c in range(w)]
 
     for i in range(w):
         for x in range(h):
-            Mbh, q = calcP(Mbh_RLO, Mdon_RLO[i][x], P_RLO[i][x], 0, 0, params[2])
-        
+            Mbh, q = calcP(Mbh_RLO, Mdon_RLO[i][x], P_RLO[i][x], alpha, beta, params[2])
+
             color_mbh[i][x] = (distance(Mbh, params[0][0], params[0][1]))
             color_q[i][x] = (distance(q, params[1][0], params[1][1]))
 
-            if (abs(color_mbh[i][x]) <= 2 and abs(color_q[i][x]) <= 2):
+            if (abs(color_mbh[i][x]) <= 2 and abs(color_q[i][x] <= 2)):
                 passes[i][x] = 1
+    colors = {'Mbh': color_mbh, 'q': color_q}
 
-    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig, axs = plt.subplots(1, 2)
     fig.set_size_inches(17, 8)
-    fig.suptitle('Mbh_RLO: ' + str(n))
+    fig.suptitle('{}\n Mbh_RLO = {}, alpha = {}, beta = {}'.format(system, n, alpha, beta))
 
-    sc1 = ax1.pcolormesh(Mdon_RLO, P_RLO, color_mbh, cmap="RdBu_r", norm=norm)
-    cn1 = ax1.contour(Mdon_RLO, P_RLO, color_mbh, colors='k', levels=(-2, 0, 2), linestyles='solid')
-    ax1.contour(Mdon_RLO, P_RLO, passes, colors='w', levels=[1], linestyles='dashed')
-    ax1.clabel(cn1, inline=1, fontsize=10, fmt='%1.0f')
-    ax1.set_title('Mbh deviation')
-    ax1.set_xlabel('Mdon_RLO')
-    ax1.set_ylabel('P_RLO')
-    cb1 = fig.colorbar(sc1, ax = ax1, ticks = bounds, extend='both')
-    cb1.set_label('standard deviations away')
-
-    sc2 = ax2.pcolormesh(Mdon_RLO, P_RLO, color_q, cmap="RdBu_r", norm=norm)
-    cn2 = ax2.contour(Mdon_RLO, P_RLO, color_q, colors='k', levels=(-2, 0, 2), linestyles='solid')
-    ax2.contour(Mdon_RLO, P_RLO, passes, colors='w', levels=[1], linestyles='dashed')
-    ax2.clabel(cn2, inline=1, fontsize=10, fmt='%1.0f')
-    ax2.set_title('q deviation')
-    ax2.set_xlabel('Mdon_RLO')
-    ax2.set_ylabel('P_RLO')
-    cb2 = fig.colorbar(sc2, ax = ax2, ticks = bounds, extend='both')
-    cb2.set_label('standard deviations away')
+    for ax, shade in zip(axs, ('Mbh', 'q')):
+        sc = ax.contourf(Mdon_RLO, P_RLO, colors[shade], cmap = "RdBu_r", norm=norm, levels = np.arange(-5, 5.5, .5), extend='both')
+        cn = ax.contour(Mdon_RLO, P_RLO, colors[shade], colors='k', levels=[-2, 0, 2], linestyles='solid')
+        ax.contour(Mdon_RLO, P_RLO, passes, colors='w', levels = [1], linewidths=2.5)
+        ax.clabel(cn, inline=1, fontsize=10, fmt='%1.0f')
+        ax.set_title(shade + ' deviation')
+        ax.set_xlabel('Mdon_RLO')
+        ax.set_ylabel('P_RLO')
+        cb = fig.colorbar(sc, ax = ax, ticks = bounds, extend='both')
+        cb.set_label('N_sigma')
     
     plt.savefig('div_plots/fig' + str(n), bbox_inches='tight')
     print('plotted ' + str(n))
     plt.close()
+
+system = 'GRS1915'
+params = get_params(system)
+
+norm = matplotlib.colors.Normalize(vmin=-5.,vmax=5.)
+bounds = range(-5, 6)
+
+alpha = 0
+beta = 1
+
+for n in range(2, 17):
+    plot(n)
